4b3d7c06b26b2a0a4e7415ac025cb3a835e56e24
whaleys1 <whaleys1@gmail.com>
2016-09-27 19:54:31 -0400

    Completed exercise 3 from Chap 2

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,16 @@
+#
+# Template for code submission
+# name : Sean Whaley
+# email : smw100@pitt.edu
+# date : 9/27/2016
+# class : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description: My first (or 9th) python program
+# description of this file goes here
+# Example: Starting with Python, Chapter 1, Exercise
+3
+#
+# Notes:
+# Hopefully you guys can see this!
+# ...and now let's program with Python
