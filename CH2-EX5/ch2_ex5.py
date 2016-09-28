de0a320c2cf921909dfea52627189c673c554066
whaleys1 <whaleys1@gmail.com>
2016-09-27 20:05:28 -0400

    Completed exercise 3 from Chap 2

---
+++ CH2-EX5/ch2_ex5.py
@@ -0,0 +1,23 @@
+# Template for code submission
+# name          : Sean Whaley
+# email         : smw100@pitt.edu
+# date          : 9/14/2016
+# class         : CS0008-f2016
+# instructor    : Max Novelli (man8@pitt.edu)
+#
+# Description: Program to display distance over time, for various time inputs
+
+print 'The car is traveling at 90 km/h'
+speed = 90
+time = 6
+distance = speed * time
+print 'after 6 hours the car has travelled: ', distance
+time = 10
+distance = speed * time
+print 'after 10 hours the car has travelled: ', distance  # changing time = 10
+time = 15
+distance = speed * time
+print 'after 15 hours the car has travelled: ', distance  # changing time = 15
+time = 2.25
+distance = speed * time
+print 'after 2 hours and 25 minutes the car has travelled:', distance  # changing time = 2.25
