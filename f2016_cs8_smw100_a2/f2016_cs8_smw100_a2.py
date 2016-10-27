1a479843b5432a7a324e4a109f9bbaf65b832dec
whaleys1 <whaleys1@gmail.com>
2016-10-27 16:53:54 -0400

    Completed exercise 3 from Chap 2

--- f2016_cs8_smw100_a2/f2016_cs8_smw100_a2.py
+++ f2016_cs8_smw100_a2/f2016_cs8_smw100_a2.py
@@ -1,12 +1,21 @@
-while file and file != 'quit' or != 'q'
+def main ():
+  tcount = 0 #intializing total line accumulator
+  tmiles = 0 #intializing total miles accumulator
+  file = input("Please enter a file name or type q for quit: ")
+  while file and file != 'quit' or != 'q':  #creating a user controlled loop for input
     fh = open (file, 'r')
-def processf(fh):
-pcount = 0
-pmiles = 0
-file = open (file_name, 'r')
-for line in file:
-    pcount += 1
-    line = line.rstrip("/n")
-    temp = line.split(",")
-    miles = float(temp [1])
-    pmiles += miles
+
+  pcount = 0  #intializing partial line accumulator
+  pmiles = 0  #intializing partial miles accumulator
+  def processfile(fh):  #defining processfile function
+    for line in fh:
+     pcount += 1   #incrementing the line count
+     line = line.rstrip("/n")  #stripping the new line character
+     temp = line.split(",")    #seperating the name from miles
+     miles = float(temp [1])   #casting miles as a float with an index to the correct data
+     pmiles += miles    #adding the partial miles to the accumulator
+ processfile(fh)   # calling processfile
+  tcount += pcount   #adding the partials to the total
+  tmiles += pmiles
+main()
+print (tcount, tmiles)
