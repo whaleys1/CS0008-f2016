61072f7d315439af29d0bc3af5b25a169e044421
whaleys1 <whaleys1@gmail.com>
2016-10-27 22:36:24 -0400

    troubleshooting file statement

--- f2016_cs8_smw100_a2/f2016_cs8_smw100_a2.py
+++ f2016_cs8_smw100_a2/f2016_cs8_smw100_a2.py
@@ -1,9 +1,9 @@
 def main():
     tcount = 0  # intializing total line accumulator
     tmiles = 0  # intializing total miles accumulator
-    file = input("Please enter a file name or type q for quit: ")
     pcount = 0  # intializing partial line accumulator
     pmiles = 0  # intializing partial miles accumulator
+    file = input("Please enter a file name or type q for quit: ")
     while file and file != 'quit' != 'q':  # creating a user controlled loop for input
         fh = open(file, 'r')
         file = input("Please enter a file name or type q for quit: ")
@@ -16,21 +16,24 @@ def main():
             temp = line.split(",")  # seperating the name from miles
             miles = float(temp[1])  # casting miles as a float with an index to the correct data
             pmiles += miles  # adding the partial miles to the accumulator
+            print ("File to be read: ",file)
+            print ("Partial Total # of lines: ",format( pcount, '10s'))
+            print ("Partial Distance run    : ",format( pmiles, '10.3f'))
             fh.close()
         processfile(fh)  # calling processfile
     tcount += pcount  # adding the partials to the total
     tmiles += pmiles
-        def printKV(key, value, klen = 0):
-            kl =  max(len(key), klen)
-            if isinstance (value, str):
-                fs = '20s'
-            elif isinstance (value, float):
-                fs = '10.3f'
-            elif isinstance (value, int):
-                fs = '10s'
-            else:
-                print ('Error! I cannot print this')
-            print (format (key, str(kl) + 's'))
+    #def printKV(key, value, klen = 0):
+            #kl =  max(len(key), klen)
+            #if isinstance (value, str):
+                #fs = '20s'
+            #elif isinstance (value, float):
+                #fs = '10.3f'
+            #elif isinstance (value, int):
+                #fs = '10s'
+            #else:
+                #print ('Error! I cannot print this')
+            #print (format (key, str(kl) + 's'))
 
     print (tcount, tmiles)
 main()
