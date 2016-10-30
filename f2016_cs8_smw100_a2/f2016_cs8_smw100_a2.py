98fb5e2bfbafc327de69db18f4cf316aea8d9310
whaleys1 <whaleys1@gmail.com>
2016-10-29 20:54:29 -0400

    Moving the close statement to the while loop

--- f2016_cs8_smw100_a2/f2016_cs8_smw100_a2.py
+++ f2016_cs8_smw100_a2/f2016_cs8_smw100_a2.py
@@ -1,20 +1,20 @@
 def main():
     tcount = 0  # intializing total line accumulator
     tmiles = 0  # intializing total miles accumulator
-    pcount = 0  # intializing partial line accumulator
-    pmiles = 0  # intializing partial miles accumulator
     file = input("Please enter a file name or type q for quit: ")
     fh = open(file, 'r')
 
     def processfile(fh):  # defining processfile function
       for line in fh:  #for loop through each line in fh
+       pcount = 0   #intializing partial accumulator
+       pmiles = 0   #intializing partial accumulator
        pcount += 1  # incrementing the line count
        line = line.rstrip("/n")  # stripping the new line character
        temp = line.split(",")  # seperating the name from miles
        miles = float(temp[1])  # casting miles as a float with an index to the correct data
        pmiles += miles  # adding the partial miles to the accumulator
        print ("File to be read: ", file)   #last ditch effort to capture some output
-       print ("Partial Total # of lines: ", format(pcount, '10s'))
+       print ("Partial Total # of lines: ", format(pcount, '10s')
        print ("Partial Distance run    : ", format(pmiles, '10.3f'))
 
 
@@ -35,4 +35,5 @@ def main():
     # fs = '10s'
     # else:
     # print ('Error! I cannot print this')
+
 main()
